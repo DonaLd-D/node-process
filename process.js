545b421aa2c3c7a6c1a173175fc0ca6884fd12e9
DonaLd-D <1376825153@qq.com>
2021-11-30 19:37:50 +0800

    init

---
+++ process.js
@@ -0,0 +1,6 @@
+const http = require('http');
+
+http.createServer().listen(3000, () => {
+    process.title = '测试进程 Node.js' // 进程进行命名
+    console.log(`process.pid: `, process.pid); // process.pid: 20279
+});
